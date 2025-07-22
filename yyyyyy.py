c763bf2ab593c6338e06cd248549d8880ce82e7a
yachika-rajput <yachika01.rajput@gmail.com>
2025-07-22 22:38:02 +0530

    Delete yyyyyy.py

--- yyyyyy.py
+++
@@ -1,7 +0,0 @@
-'''name  = input("enter your name: ")
-print(f"good afternoon {name}")'''
-
-letter = ''' Dear "<|name|>,
-you are selected! <|date|>...'''
-
-print(letter.replace("<|name|>" , "yachi").replace("<|date|>" , "34 sept"))
